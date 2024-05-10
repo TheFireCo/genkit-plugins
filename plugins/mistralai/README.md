575dd54ad5f068b0192952a40c03e286b74b877a
David Alonso <davidoort@hotmail.com>
2024-05-10 16:01:30 +0200

    add mistralai plugin skeleton

---
+++ plugins/mistralai/README.md
@@ -0,0 +1,38 @@
+<h1 align="center">Firebase Genkit <> Cohere Plugin</h1>
+
+<h4 align="center">Cohere Community Plugin for Google Firebase Genkit</h4>
+
+<div align="center"></div>
+
+`genkitx-cohere` is a community plugin for using Cohere and all its supported models with [Firebase GenKit](https://github.com/firebase/genkit).
+
+## Installation
+
+Install the plugin in your project with your favorite package manager:
+
+- `npm install genkitx-cohere`
+- `yarn add genkitx-cohere`
+
+## Usage
+
+> \[!WARNING\]\
+> Documentation is currently work in progress.
+
+## Contributing
+
+Want to contribute to the project? That's awesome! Head over to our [Contribution Guidelines](CONTRIBUTING.md).
+
+## Need support?
+
+> \[!NOTE\]\
+> This repository depends on Google's Firebase Genkit. For issues and questions related to GenKit, please refer to instructions available in [GenKit's repository](https://github.com/firebase/genkit).
+
+Reach out by opening a discussion on [Github Discussions](https://github.com/TheFireCo/genkitx-openai/discussions).
+
+## Credits
+
+This plugin is proudly maintained by the team at [**The Fire Company**](https://github.com/TheFireCo). 🔥
+
+## License
+
+This project is licensed under the [Apache 2.0 License](https://github.com/TheFireCo/genkitx-openai/blob/main/LICENSE).
---
+++ plugins/mistralai/src/index.ts
@@ -0,0 +1,43 @@
+
+/**
+ * Copyright 2024 The Fire Company
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { genkitPlugin, Plugin } from '@genkit-ai/core';
+import MistralClient from '@mistralai/mistralai';
+
+export interface PluginOptions {
+  apiKey?: string;
+}
+
+export const mistralai: Plugin<[PluginOptions] | []> = genkitPlugin(
+  'mistralai',
+  async (options?: PluginOptions) => {
+    let apiKey = options?.apiKey || process.env.MISTRALAI_API_KEY;
+    if (!apiKey)
+      throw new Error(
+        'Please pass in the API key or set the MISTRALAI_API_KEY environment variable'
+      );
+    const client = new MistralClient(apiKey);
+    return {
+      models: [
+        // TODO: Add Mistral AI models as needed
+      ],
+    };
+  }
+);
+
+export default mistralai;
+
