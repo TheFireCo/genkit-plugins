bc0aa36cedad78e64b2a517eb858805064643cd4
David Alonso <davidoort@hotmail.com>
2024-05-10 15:45:35 +0200

    add cohere plugin skeleton

---
+++ plugins/cohere/src/index.ts
@@ -0,0 +1,45 @@
+
+/**
+ * Copyright 2024 The Fire Company
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { genkitPlugin, Plugin } from '@genkit-ai/core';
+import { CohereClient } from 'cohere-ai';
+
+export interface PluginOptions {
+  apiKey?: string;
+}
+
+export const cohere: Plugin<[PluginOptions] | []> = genkitPlugin(
+  'cohere',
+  async (options?: PluginOptions) => {
+    let apiKey = options?.apiKey || process.env.COHERE_API_KEY;
+    if (!apiKey)
+      throw new Error(
+        'Please pass in the API key or set the COHERE_API_KEY environment variable'
+      );
+    const client = new CohereClient({ token: apiKey });
+    return {
+      models: [
+        // Example: Add support for Cohere models
+        // { name: 'generate', model: client.generate },
+        // { name: 'embed', model: client.embed },
+        // Add other Cohere models as needed
+      ],
+    };
+  }
+);
+
+export default cohere;
