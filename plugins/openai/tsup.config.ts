0010bd4d27f7730cb2601f7ea4439d21f9ee6003
David Alonso <davidoort@hotmail.com>
2024-05-10 15:28:44 +0200

    small update to ownership string

--- plugins/openai/tsup.config.ts
+++ plugins/openai/tsup.config.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Giftit Social, Inc.
+ * Copyright 2024 The Fire Company
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
