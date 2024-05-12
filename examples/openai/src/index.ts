0b2be4820db8530ad577451897942cb962e8f8ac
Vukasin Bozic <bozic.vukasin@gmail.com>
2024-05-12 18:11:44 +0200

    examples done, just filling READMEs

--- examples/openai/src/index.ts
+++ examples/openai/src/index.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config';
 
 import { defineDotprompt, dotprompt, prompt } from '@genkit-ai/dotprompt';
-import { generate, definePrompt, renderPrompt } from '@genkit-ai/ai';
-import { configureGenkit } from '@genkit-ai/core';
+import { generate, definePrompt, renderPrompt, defineTool } from '@genkit-ai/ai';
+import { configureGenkit, action } from '@genkit-ai/core';
 import { defineFlow, runFlow, startFlowsServer } from '@genkit-ai/flow';
 import { openAI } from 'genkitx-openai-plugin';
 import * as z from 'zod';
-
 export default configureGenkit({
   plugins: [
     /* Add your plugins here. */
@@ -50,15 +49,6 @@ const helloPrompt = definePrompt(
 //   })
 // );
 
-// Run the standard prompt 
-// generate(
-//   renderPrompt({
-//     prompt: helloPrompt,
-//     input: { name: 'Fred' },
-//     model: 'openai/gpt-3.5-turbo',
-//   })
-// );
-
 // define Dotprompts
 const greetingPrompt = prompt('basic');
 const multimodalPrompt = prompt('multimodalInput');
@@ -72,28 +62,57 @@ const customConfigPrompt = prompt('customConfig');
 const codeDotPrompt = defineDotprompt(
   {
     name: 'exampleDotPrompt',
-    model: 'openai/gpt-3.5-turbo',
+    model: 'openai/gpt-4-turbo',
     input: {
       schema: z.object({
-        name: z.string(),
+        object_name: z.string(),
+        image_url: z.string(),
+      }),
+    },
+    output: {
+      schema: z.object({
+        exist: z.boolean(),
+        color: z.string(),
+        details: z.string(),
       }),
     },
   },
-  `Hello {{name}}, how are you today?`
+  `Does the object {{object_name}} exist in the given image {{media url=image_url}}? If it does, what color is it and what are some details about it?`
 );
 
 // Run the dotprompt
 // const response = codeDotPrompt.generate({
 //  input:{
-//       name: 'Fred',
+//       object_name: 'Ball',
+//       image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTye8WtmgfLgizN554CJWJvenpm_ZLGqXElT5NHcV2EQU_h1Hak9lHvczZsyA&s',
 //     }
 //   }
 // );
+
 // For the response, you can use the following to get the text
 // console.log(response.text());
 // Or you can use the following to get the structured output, if you specified it
 // console.log(response.output());
 
+// Tool calling
+const tool = defineTool(
+  {
+    name: 'myJoke',
+    description: 'useful when you need a joke to tell.',
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (input) => `haha Just kidding no joke about for you! got you`
+);
+
+// const llmResponse = generate({
+//   model: 'openai/gpt-3.5-turbo',
+//   prompt: 'Tell me a joke.',
+//   tools: [tool],
+//   config: {
+//     temperature: 0.5,
+//   },
+// });
 
 // Define flows
 
